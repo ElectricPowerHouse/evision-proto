41d01761714fa6489feda25080ba36bdd31e66f3
Jack Upton <acidplaystf2@gmail.com>
2022-03-28 17:31:46 +1300

    added 2 videos on page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Modal from 'react-modal';
+import Draggable from 'react-draggable';
+
+
+
 
 function App() {
   return (
     <div className="App">
+      <div className = "Navbar">
+      </div>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <Draggable>
+      <div className="Overlay-Window">
+      This is the overlay video
+
+      <video className="Video2" width='1000' height='2000' controls>
+        <source src="https://www.w3schools.com/html/mov_bbb.mp4" type="video/mp4" />
+        <source src="movie.ogg" type="video/ogg" />
+        video not work
+      </video>
+      </div>
+       </Draggable>
+
+      <video className="Video" width='1000' height='2000' controls>
+        <source src="https://www.w3schools.com/html/mov_bbb.mp4" type="video/mp4" />
+        <source src="movie.ogg" type="video/ogg" />
+        video not work
+      </video>
+
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        The merchant of Venice
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
+
     </div>
   );
 }
